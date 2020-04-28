615e59c69097a589431081391d1da04fdf3d6130
ELezov <eugene.lezov@gmail.com>
2020-04-28 15:29:46 +0200

    small changes

--- Example/EasyDeviceUtils/ViewController.swift
+++ Example/EasyDeviceUtils/ViewController.swift
@@ -7,18 +7,53 @@
 //
 
 import UIKit
+import EasyDeviceUtils
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var strings: [String] = []
+    
+    var deviceInfo: [String: String] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let device = EasyDeviceUtils.shared
+        deviceInfo["App Identifier"] = device.appIdentifier ?? ""
+        deviceInfo["Locale code"] = device.localeCode ?? ""
+        deviceInfo["Battery level"] = device.batteryInfo.level.description
+        deviceInfo["Battery state"] = String(device.batteryInfo.state.rawValue)
+        deviceInfo["User device name"] = device.userDeviceName
+        deviceInfo["Model device name"] = device.modelInfo.modelDevice
+        deviceInfo["Model name"] = device.modelInfo.modelString
+        deviceInfo["Mobile country code"] = device.carrierInfo.mobileCountryCode
+        deviceInfo["Mobile provider name"] = device.carrierInfo.providerName
+        deviceInfo["Device screen brightness"] = "\(device.screen.brightness)"
+        deviceInfo["Device wifi ssid"] = device.wifiInfo.ssid
 
+        strings = deviceInfo.map { "\($0.key): \($0.value)" }
+        tableView.dataSource = self
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell")
+        
+        cell?.textLabel?.text = strings[indexPath.row]
+        cell?.textLabel?.font = UIFont.italicSystemFont(ofSize: 12)
+        return cell ?? UITableViewCell()
+    }
 }
 
