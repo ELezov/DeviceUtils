0adbfdb8cc16205638c5ff2088b135a5ff3dc905
ELezov <eugene.lezov@gmail.com>
2020-04-29 20:52:07 +0200

    start refactoring

--- EasyDeviceUtils/Classes/BatteryInfoModel.swift
+++ EasyDeviceUtils/Classes/BatteryInfoModel.swift
@@ -8,20 +8,58 @@
 
 import UIKit
 
+// https://developer.apple.com/documentation/uikit/uidevice/1620051-batterystate?language=swift
+
 public struct BatteryInfoModel {
+    
+    enum BatteryState {
+        // If battery monitoring is not enabled
+        case unknown
+        // The device is not plugged into power; the battery is discharging.
+        case unplugged(Int)
+        // The device is plugged into power and the battery
+        // is less than 100% charged.
+        case charging(Int)
+        // The device is plugged into power and the battery is 100% charged.
+        case full(Int)
+        /// BatteryLevel return - 1
+        case simulator
+    }
+    
+    var state: BatteryState {
+        let level = UIDevice.current.batteryLevel
+        let batteryState = UIDevice.current.batteryState
+        
+        let levelInPercent = Int(round(level * 100))
+        
+        switch batteryState {
+        case .unknown:
+            return level == -1.0 ? .simulator : .unknown
+        case .unplugged:
+            return .unplugged(levelInPercent)
+        case .charging:
+            return .charging(levelInPercent)
+        case .full:
+            return .full(levelInPercent)
+        @unknown default:
+            assertionFailure()
+            return .unknown
+        }
+    }
+    
+    var batteryLevel: Int {
+        switch state {
+        case .charging(let level),
+             .unplugged(let level),
+             .full(let level):
+            return level
+        case .simulator,
+             .unknown:
+            return 100
+        }
+    }
     
     init() {
         UIDevice.current.isBatteryMonitoringEnabled = true
-    }
-    
-    // The battery state for the device. (unknown, unplugged, charging, full)
-    public var state: UIDevice.BatteryState {
-        return UIDevice.current.batteryState
-    }
-    
-    // The battery charge level for the device.
-    // in range 0...1, -1 for simulators
-    public var level: Float {
-        return UIDevice.current.batteryLevel
     }
 }
